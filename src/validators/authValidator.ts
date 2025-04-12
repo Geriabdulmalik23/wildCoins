a541923dd03acb93ddfb67ebaada314d976b3bcd
Geri Abdul Malik <geriabdulmalik4@gmail.com>
2025-04-12 17:40:50 +0700

    refactoring code

---
+++ src/validators/authValidator.ts
@@ -0,0 +1,9 @@
+import { password } from 'bun'
+import { z } from 'zod'
+
+export const loginSchema = z.object({
+    email : z.string().email({message: 'Email tidak valid'}),
+    password : z.string().min(6,{message : 'Password minimal 6 karakter'})
+})
+
+export type LoginIput = z.infer<typeof loginSchema>
